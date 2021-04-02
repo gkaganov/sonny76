9791263d2fa472944d180e7eb7a468c9753f41c2
gkaganov <gkaganov@github>
2021-04-02 03:58:25 +0200

    stuff

--- app/Main.hs
+++ app/Main.hs
@@ -32,6 +32,7 @@ data Hero =
   Hero
     { name :: String
     , health :: Integer
+    , focusAmount :: Integer
     , slashing :: Bool
     , dead :: Bool
     }
@@ -78,10 +79,21 @@ initialModel :: Model
 initialModel =
   Model
     { player =
-        Hero {name = "bro", health = 1000, slashing = False, dead = False}
+        Hero
+          { name = "bro"
+          , health = 1000
+          , focusAmount = 100
+          , slashing = False
+          , dead = False
+          }
     , enemy =
         Hero
-          {name = "the baddies", health = 1000, slashing = False, dead = False}
+          { name = "the baddies"
+          , health = 1000
+          , focusAmount = 100
+          , slashing = False
+          , dead = False
+          }
     , battleFinished = False
     , playerTurn = True
     }
@@ -107,10 +119,11 @@ updateModel NoOp m = noEff m
 updateModel (Print t) m = m <# do liftIO (putStrLn t) >> pure NoOp
 updateModel (Slash ht) m =
   noEff $
-  checkIfBattleFinished $
-  handleAnimation (Slash ht) $ handleDamage (Slash ht) $ m {playerTurn = False}
+  handleAnimation (Slash ht) $
+  handleFocusCost (Slash ht) $ handleDamage (Slash ht) $ m {playerTurn = False}
 updateModel (SlashEnd ht) m =
   noEff $
+  checkIfBattleFinished $
   handleAnimation
     (SlashEnd ht)
     (case ht of
@@ -134,15 +147,23 @@ handleAnimation _ m = m
 handleDamage :: Action -> Model -> Model
 handleDamage (Slash herotype) m =
   case herotype of
-    Player -> m {enemy = applyDamage (enemy m) 100}
-    Enemy -> m {player = applyDamage (player m) 100}
+    Player -> m {enemy = applyDamage (enemy m) 200}
+    Enemy -> m {player = applyDamage (player m) 150}
 handleDamage _ m = m
+
+handleFocusCost :: Action -> Model -> Model
+handleFocusCost (Slash herotype) m =
+  case herotype of
+    Player -> m {player = subtractFocus (player m) 20}
+    Enemy -> m {enemy = subtractFocus (enemy m) 10}
+handleFocusCost _ m = m
 
 handleEnemyTurn :: Model -> Model
 handleEnemyTurn m =
   if dead $ enemy m
     then m
-    else handleAnimation (Slash Enemy) $ handleDamage (Slash Enemy) m
+    else handleFocusCost (Slash Enemy) $
+         handleAnimation (Slash Enemy) $ handleDamage (Slash Enemy) m
 
 applyDamage :: Hero -> Integer -> Hero
 applyDamage hero damage = checkIfDead $ hero {health = health hero - damage}
@@ -152,6 +173,9 @@ checkIfDead hero =
   if health hero <= 0
     then hero {dead = True, health = 0}
     else hero
+
+subtractFocus :: Hero -> Integer -> Hero
+subtractFocus hero cost = hero {focusAmount = focusAmount hero - cost}
 
 checkIfBattleFinished :: Model -> Model
 checkIfBattleFinished m =
@@ -200,13 +224,28 @@ viewModel m =
                   M.fromList [("width", "var(--health-bar-container-width)")]
                 ]
                 [ div_
-                    [class_ "health-text player"]
+                    [class_ "health-amount player"]
                     [text $ ms $ show $ health $ player m]
                 , div_
                     [ class_ "health-bar player"
                     , classList_ [calculateHealthBarColorLabel $ player m]
                     , style_ $
                       M.fromList [("width", buildHealthBarWidth m Player)]
+                    ]
+                    []
+                ]
+            , div_
+                [ class_ "focus-bar-container"
+                , style_ $
+                  M.fromList [("width", "var(--focus-bar-container-width)")]
+                ]
+                [ div_
+                    [class_ "focus-amount player"]
+                    [text $ ms $ show $ focusAmount $ player m]
+                , div_
+                    [ class_ "focus-bar player"
+                    , style_ $
+                      M.fromList [("width", buildFocusBarWidth m Player)]
                     ]
                     []
                 ]
@@ -228,13 +267,28 @@ viewModel m =
                   M.fromList [("width", "var(--health-bar-container-width)")]
                 ]
                 [ div_
-                    [class_ "health-text enemy"]
+                    [class_ "health-amount enemy"]
                     [text $ ms $ show $ health $ enemy m]
                 , div_
                     [ class_ "health-bar enemy"
                     , classList_ [calculateHealthBarColorLabel $ enemy m]
                     , style_ $
                       M.fromList [("width", buildHealthBarWidth m Enemy)]
+                    ]
+                    []
+                ]
+            , div_
+                [ class_ "focus-bar-container"
+                , style_ $
+                  M.fromList [("width", "var(--focus-bar-container-width)")]
+                ]
+                [ div_
+                    [class_ "focus-amount enemy"]
+                    [text $ ms $ show $ focusAmount $ enemy m]
+                , div_
+                    [ class_ "focus-bar enemy"
+                    , style_ $
+                      M.fromList [("width", buildFocusBarWidth m Enemy)]
                     ]
                     []
                 ]
@@ -250,21 +304,42 @@ viewModel m =
             ]
         ]
     , div_
-        [ class_ "ability-button"
-        , classList_ [("enabled", playerTurn m || battleFinished m)]
-        ]
-        [ p_
-            [ onClick $
-              if battleFinished m
-                then Restart
-                else if playerTurn m
-                       then Slash Player
-                       else NoOp
+        [class_ "horizontal ability-bar"]
+        [ div_
+            [ class_ "ability-button"
+            , classList_ [("enabled", playerTurn m || battleFinished m)]
             ]
-            [ text $
-              if battleFinished m
-                then "restart"
-                else "slash"
+            [ p_
+                [ onClick $
+                  if battleFinished m
+                    then Restart
+                    else if playerTurn m
+                           then Slash Player
+                           else NoOp
+                ]
+                [ text $
+                  if battleFinished m
+                    then "restart"
+                    else "slash"
+                ]
+            ]
+        , div_
+            [ class_ "ability-button"
+            , classList_ [("enabled", playerTurn m || battleFinished m)]
+            ]
+            [ p_
+                [ onClick $
+                  if battleFinished m
+                    then Restart
+                    else if playerTurn m
+                           then Slash Player
+                           else NoOp
+                ]
+                [ text $
+                  if battleFinished m
+                    then "restart"
+                    else "hack"
+                ]
             ]
         ]
     ]
@@ -287,3 +362,16 @@ calculateHealthBarColorLabel hero =
   if | health hero > 500 -> ("full", True)
      | health hero > 250 -> ("medium", True)
      | otherwise -> ("low", True)
+
+buildFocusBarWidth :: Model -> HeroType -> MisoString
+buildFocusBarWidth m herotype =
+  ms
+    ("calc(" ++
+     "var(--focus-bar-width) * " ++
+     "(" ++
+     show
+       (focusAmount $
+        case herotype of
+          Player -> player m
+          Enemy -> enemy m) ++
+     "/" ++ show 100 ++ "))")
